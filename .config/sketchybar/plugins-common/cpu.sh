6bba2e4ea4d2cad7c5366e8086e216d0248c12f2
hiback <5335514+hiback@users.noreply.github.com>
2025-03-28 21:05:01 +0900

    sketchybar: fix 3 digit display for cpu usage

--- .config/sketchybar/plugins-common/cpu.sh
+++ .config/sketchybar/plugins-common/cpu.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sketchybar -m --set "$NAME" label="$(top -l 2 | grep -E "^CPU" | tail -1 | awk '{ print $3 + $5"%" }')"
+sketchybar -m --set "$NAME" label="$(top -l 2 | grep -E "^CPU" | tail -1 | awk '{usage = $3 + $5; if (usage < 10) printf "%.2f%%", usage; else printf "%.1f%%", usage}')"
