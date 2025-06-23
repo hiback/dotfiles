bff3fed5712378c527bac5c264d0fbb11572a7ce
hiback <5335514+hiback@users.noreply.github.com>
2025-06-23 14:38:47 +0900

    nvim: swap paste behavior in visual mode

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -10,6 +10,10 @@ map("i", "<c-j>", "<down>", { desc = "Move down in insert mode" })
 map("i", "<c-k>", "<up>", { desc = "Move up in insert mode" })
 map("i", "<c-l>", "<right>", { desc = "Move right in insert mode" })
 
+-- Swap paste behavior in visual mode
+map("v", "p", "P", { desc = "Paste without replacing clipboard" })
+map("v", "P", "p", { desc = "Paste and replace clipboard (classic)" })
+
 -- Code dragging in visual mode
 map("v", "J", ":m '>+1<CR>gv=gv", { desc = "Move down 1 line" })
 map("v", "K", ":m '<-2<CR>gv=gv", { desc = "Move up 1 line" })
