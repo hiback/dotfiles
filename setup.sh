0a85c7fc0da1383936dd02cc3516487d9c5443c3
hiback <5335514+hiback@users.noreply.github.com>
2026-01-05 08:57:38 +0900

    setup: add eza

--- setup.sh
+++ setup.sh
@@ -70,6 +70,9 @@ brew install starship
 # Install yazi and dependencies
 brew install yazi ffmpeg sevenzip jq poppler fd ripgrep fzf zoxide imagemagick
 
+# Install ls alternative
+brew install eza
+
 # Install html parser
 brew install pup
 
