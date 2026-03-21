23086d344e7c518cdbfc240fad1b90b728818e0d
hiback <5335514+hiback@users.noreply.github.com>
2026-03-21 13:21:16 +0900

    nvim: update & use new method to disable keymaps

--- .config/nvim/lua/plugins/disable-keymaps.lua
+++ .config/nvim/lua/plugins/disable-keymaps.lua
@@ -1,8 +1,12 @@
 return {
   "neovim/nvim-lspconfig",
-  opts = function()
-    local keys = require("lazyvim.plugins.lsp.keymaps").get()
-    -- disable ctrl+k
-    keys[#keys + 1] = { "<c-k>", false, mode = "i" }
-  end,
+  opts = {
+    servers = {
+      ["*"] = {
+        keys = {
+          { "<c-k>", false, mode = "i" },
+        },
+      },
+    },
+  },
 }
