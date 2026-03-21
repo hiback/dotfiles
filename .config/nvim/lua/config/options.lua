4df4668be25342192d2616159c0c614557a7e274
hiback <5335514+hiback@users.noreply.github.com>
2026-03-21 15:18:00 +0900

    nvim: add osc52 copy support for ssh session

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,3 +1,24 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+if vim.env.SSH_TTY then
+  vim.o.clipboard = "unnamedplus"
+  local osc52 = require("vim.ui.clipboard.osc52")
+  vim.g.clipboard = {
+    name = "OSC 52",
+    copy = {
+      ["+"] = osc52.copy("+"),
+      ["*"] = osc52.copy("*"),
+    },
+    paste = {
+      -- Use internal register for paste
+      ["+"] = function()
+        return { vim.split(vim.fn.getreg('"'), "\n"), vim.fn.getregtype('"') }
+      end,
+      ["*"] = function()
+        return { vim.split(vim.fn.getreg('"'), "\n"), vim.fn.getregtype('"') }
+      end,
+    },
+  }
+end
