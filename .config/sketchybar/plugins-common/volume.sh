86121a09d5f2176e5337555d758c0a771816ad39
hiback <5335514+hiback@users.noreply.github.com>
2026-08-14 01:15:17 +0900

    sketchybar: fix volume on startup

--- .config/sketchybar/plugins-common/volume.sh
+++ .config/sketchybar/plugins-common/volume.sh
@@ -1,6 +1,24 @@
 #!/bin/sh
 
-case ${INFO} in
+VOLUME="$INFO"
+
+# INFO is only populated by volume_change. Startup/reload uses a forced update,
+# so query macOS directly when INFO is missing (also covers device switches).
+case "$VOLUME" in
+''|*[!0-9]*)
+  VOLUME=$(osascript \
+    -e 'set volumeSettings to get volume settings' \
+    -e 'if output muted of volumeSettings then return 0' \
+    -e 'return output volume of volumeSettings' 2>/dev/null)
+  ;;
+esac
+
+# Some digital outputs do not expose software volume; keep the previous value.
+case "$VOLUME" in
+''|*[!0-9]*) exit 0 ;;
+esac
+
+case "$VOLUME" in
 0)
   ICON=""
   ICON_PADDING_RIGHT=21
@@ -15,4 +33,7 @@ case ${INFO} in
   ;;
 esac
 
-sketchybar --set $NAME icon=$ICON icon.padding_right=$ICON_PADDING_RIGHT label="$INFO%"
+sketchybar --set "$NAME" \
+  icon="$ICON" \
+  icon.padding_right="$ICON_PADDING_RIGHT" \
+  label="${VOLUME}%"
